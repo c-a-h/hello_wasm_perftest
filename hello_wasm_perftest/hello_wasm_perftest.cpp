3907740a5df8ee7a0bcf23c46302119854de3a9b
chris <chris@ANA_LAPTOP>
2023-02-21 13:02:30 -0700

    Add project files.

---
+++ hello_wasm_perftest/hello_wasm_perftest.cpp
@@ -0,0 +1,12 @@
+﻿// hello_wasm_perftest.cpp : Defines the entry point for the application.
+//
+
+#include "hello_wasm_perftest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
